4bbc6ef827139d1f4a2143abe4e32d3956125667
Venkatesha Murthy <vmurthy@falabella.cl>
2021-06-22 15:29:31 +0530

    intial commit

---
+++ android/app/src/main/kotlin/com/venkatesh/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.venkatesh.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/login_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+/**
+ * Created by venkateshmurthytp on 11,June,2021
+ */
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
